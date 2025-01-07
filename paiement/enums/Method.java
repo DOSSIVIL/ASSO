0e4ce6d517b75792e228cab3b730618c2e393273
SoniaNj <148544027+SoniaNj@users.noreply.github.com>
2025-01-07 17:24:22 +0100

    Delete paiement directory

--- paiement/enums/Method.java
+++
@@ -1,9 +0,0 @@
-package com.ecommerce.paiement.web.enums;
-
-public enum Method {
-    WALLET,
-    MTNCM,
-    ORANGECM,
-    ON_DELIVERY,
-    NULL
-}
--- paiement/enums/State.java
+++
@@ -1,8 +0,0 @@
-package com.ecommerce.paiement.web.enums;
-
-public enum State {
-    IN_PROGRESS,
-    ACCEPTED,
-    REFUSED,
-    NULL
-}
